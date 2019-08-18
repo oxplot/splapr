b0d740c1a21002a509613d15138bb3c9ee40479a
Mansour Behabadi <mansour@oxplot.com>
2019-08-19 01:05:59 +1000

    Compiles fine - not tested

--- splapr.ino
+++ splapr.ino
@@ -1,16 +1,28 @@
 #include <SoftSerial.h>
+#include <FastCRC.h>
 
 #define RX_PIN 5
 #define TX_PIN 4
-#define MOT_1_PIN 1
-#define MOT_2_PIN 3
-#define MOT_3_PIN 2
-#define MOT_4_PIN 4
-#define POS_SENSE_PIN 0
+#define SERIAL_BAUD 9600
+
+#define MOT_1_PIN 1 // TODO color
+#define MOT_2_PIN 3 // TODO color
+#define MOT_3_PIN 2 // TODO color
+#define MOT_4_PIN 4 // TODO color
 #define MOT_STEPS 2038L // the number of steps in one revolution of the 28BYJ-48 motor
 #define MODULE_STEPS ((MOT_STEPS * 39L) / 32L) // 39:32 gear ratio
 
+#define POS_SENSE_PIN 0
+
+#define STATUS_MOVING 1
+#define STATUS_IDLE 0
+#define POS_UNINIT ((1L << 12) - 1L) // 12 ones binary
+
 SoftSerial serial(RX_PIN, TX_PIN, true /* inverse logic */);
+FastCRC8 CRC8;
+
+bool curStatus;
+long targetPosition = POS_UNINIT;
 
 void setup() {
   pinMode(POS_SENSE_PIN, INPUT);
@@ -18,15 +30,64 @@ void setup() {
   pinMode(MOT_2_PIN, OUTPUT);
   pinMode(MOT_3_PIN, OUTPUT);
   pinMode(MOT_4_PIN, OUTPUT);
-  serial.begin(9600);
+  serial.begin(SERIAL_BAUD);
 }
 
 void loop() {
-  for (int i = 0; i < 10; i++) {
-    stepOnceTowards(2000);
+  if (targetPosition != POS_UNINIT) {
+    curStatus = stepOnceTowards(targetPosition) ? STATUS_IDLE : STATUS_MOVING;
   }
+  handleIncoming();
 }
 
+void handleIncoming() {
+  // Packet buffer includes 3 bytes of data as descibed below and one byte of checksum.
+  // Packet structure:
+  // Byte 0, bits 0-7: MSBits of Src/Dst
+  // Byte 1, bits 6-7: LSBits of Src/Dst
+  // Byte 1, bits 0-5: MSBits of Position (ignored for response packet)
+  // Byte 2, bits 2-7: LSBits of Position (ignored for response packet)
+  // Byte 2, bit 1:    Packet Type (0: response, 1: command)
+  // Byte 2, bit 0:    Status (0: idle, 1: moving)
+  // Byte 3:           SMBUS CRC-8 checksum
+  static byte buf[4] = {0, 0, 0, 0};
+  
+  if (!serial.available()) {
+    return;
+  }
+
+  buf[0] = buf[1];
+  buf[1] = buf[2];
+  buf[2] = buf[3];
+  buf[3] = serial.read();
+
+  if (CRC8.smbus(buf, 3) != buf[3]) {
+    return;
+  }
+
+  // Extract packet data
+  unsigned long srcDst = ((unsigned long)buf[0] << 2) | ((unsigned long)buf[1] >> 6);
+  bool cmdPacket = ((unsigned long)buf[2] >> 1) & 1L;
+
+  if (srcDst == 0) { // This packet was destined for us
+    if (!cmdPacket) { // Invalid packet - must be a command
+      return;
+    }
+    buf[2] = (unsigned long)buf[2] & ~0b10L; // Change packet type to response
+    buf[2] = ((unsigned long)buf[2] & ~1L) | (curStatus == STATUS_IDLE ? 0L : 1L); // Set current status
+    targetPosition = ((((1L << 6) - 1L) & (unsigned long)buf[1]) << 6) | ((unsigned long)buf[2] >> 2); // Update target position
+  }
+
+  // Update src/dst & CRC
+  srcDst = srcDst + (cmdPacket ? -1L : 1L);
+  buf[3] = CRC8.smbus(buf, 3);
+
+  // Send out packet & clear buffer for future receptions.
+  serial.write(buf, 4);
+  buf[0] = buf[1] = buf[2] = buf[3] = 0;
+}
+
+// Returns true if we've arrived at loc, otherwise false.
 bool stepOnceTowards(long loc) {
   static long realLoc = -1L;
   static long normalizedLoc = 0L;
