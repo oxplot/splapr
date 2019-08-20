4085996c29b09b462b86ee7a12d35611577d2b35
Mansour Behabadi <mansour@oxplot.com>
2019-08-20 22:26:39 +1000

    Comms test

--- splapr.ino
+++ splapr.ino
@@ -1,24 +1,44 @@
+#ifdef ARDUINO_AVR_NANO
+// Nano is used for development and testing
+  #define DEV
+#endif
+
+#ifndef DEV
 #include <SoftSerial.h>
+#endif
+
 #include <FastCRC.h>
 
-#define RX_PIN 5
-#define TX_PIN 4
-#define SERIAL_BAUD 9600
+#ifdef DEV
+  #define TX_PIN 5
+  #define MOT_1_PIN 2 // Blue wire
+  #define MOT_2_PIN 4 // Pink wire
+  #define MOT_3_PIN 3 // Yellow wire
+  #define MOT_4_PIN TX_PIN // Orange wire
+  #define POS_SENSE_PIN 7
+  
+  #define serial Serial
+#else
+  #define RX_PIN 5
+  #define TX_PIN 4
+  #define MOT_1_PIN 0 // Blue wire
+  #define MOT_2_PIN 2 // Pink wire
+  #define MOT_3_PIN 1 // Yellow wire
+  #define MOT_4_PIN TX_PIN // Orange wire
+  #define POS_SENSE_PIN 3
+#endif
 
-#define MOT_1_PIN 0 // TODO color
-#define MOT_2_PIN 2 // TODO color
-#define MOT_3_PIN 1 // TODO color
-#define MOT_4_PIN 4 // TODO color
+#define SERIAL_BAUD 9600
 #define MOT_STEPS 2038L // the number of steps in one revolution of the 28BYJ-48 motor
 #define MODULE_STEPS ((MOT_STEPS * 39L) / 32L) // 39:32 module gear ratio
-
-#define POS_SENSE_PIN 3
-
 #define STATUS_MOVING 1
 #define STATUS_IDLE 0
 #define POS_UNINIT ((1L << 12) - 1L) // 12 ones binary
 
+#ifndef DEV
 SoftSerial serial(RX_PIN, TX_PIN, true /* inverse logic */);
+#endif
+
 FastCRC8 CRC8;
 
 bool curStatus;
@@ -30,6 +50,7 @@ void setup() {
   pinMode(MOT_2_PIN, OUTPUT);
   pinMode(MOT_3_PIN, OUTPUT);
   pinMode(MOT_4_PIN, OUTPUT);
+
   serial.begin(SERIAL_BAUD);
 
   // Turn all output off by default
@@ -37,6 +58,7 @@ void setup() {
   digitalWrite(MOT_2_PIN, LOW);
   digitalWrite(MOT_3_PIN, LOW);
   digitalWrite(MOT_4_PIN, LOW);
+
 }
 
 void loop() {
@@ -49,7 +71,7 @@ void loop() {
 void handleIncoming() {
   // Packet buffer includes 3 bytes of data as descibed below and one byte of checksum.
   // Packet structure:
-  // Byte 0:           Constant 0xd4 as packet start. Also helps with avoiding valid packets of all zeros.
+  // Byte 0:           Constant 0xd4 as packet header. Also helps with avoiding valid packets of all zeros.
   // Byte 1, bits 0-7: MSBits of Src/Dst
   // Byte 2, bits 6-7: LSBits of Src/Dst
   // Byte 2, bits 0-5: MSBits of Position (ignored for response packet)
@@ -57,18 +79,19 @@ void handleIncoming() {
   // Byte 3, bit 1:    Packet Type (0: response, 1: command)
   // Byte 3, bit 0:    Status (0: idle, 1: moving)
   // Byte 4:           SMBUS CRC-8 checksum
-  static byte buf[5] = {0xd4, 0, 0, 0, 0};
+  static byte buf[5] = {0, 0, 0, 0, 0};
   
   if (!serial.available()) {
     return;
   }
 
+  buf[0] = buf[1];
   buf[1] = buf[2];
   buf[2] = buf[3];
   buf[3] = buf[4];
   buf[4] = (byte)serial.read();
 
-  if (CRC8.smbus(buf, 4) != buf[4]) {
+  if (buf[0] != 0xd4 || CRC8.smbus(buf, 4) != buf[4]) {
     return;
   }
 
@@ -77,15 +100,10 @@ void handleIncoming() {
   bool cmdPacket = ((unsigned long)buf[3] >> 1) & 1L;
   unsigned long pos = ((((1L << 6) - 1L) & (unsigned long)buf[2]) << 6) | ((unsigned long)buf[3] >> 2);
 
-  serial.print("src/dst = ");
-  serial.println(srcDst);
-  serial.print("target pos = ");
-  serial.println(targetPosition);
-
   if (srcDst == 0) { // This packet was destined for us
     if (!cmdPacket) { // Invalid packet - must be a command
       return;
-    }
+    }:
     buf[3] = (unsigned long)buf[3] & ~0b10L; // Change packet type to response
     cmdPacket = false;
     buf[3] = ((unsigned long)buf[3] & ~1L) | (curStatus == STATUS_IDLE ? 0L : 1L); // Set current status
@@ -99,7 +117,7 @@ void handleIncoming() {
   buf[4] = CRC8.smbus(buf, 4);
 
   // Send out packet & clear buffer for future receptions.
-  serial.write(buf + 1, 4);
+  serial.write(buf, 5);
 }
 
 // Returns true if we've arrived at loc, otherwise false and step forward.
--- test.go
+++ test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/sigurn/crc8"
 	"github.com/tarm/serial"
@@ -9,6 +10,44 @@ import (
 
 var crcSMBUSParams = crc8.Params{0x07, 0x00, false, false, 0x00, 0xf4, "CRC-8/SMBUS"}
 var maximCRCTable = crc8.MakeTable(crcSMBUSParams)
+
+type packetType int
+type moduleStatus int
+
+const (
+	packetTypeCommand  = 1
+	packetTypeResponse = 0
+
+	moduleStatusIdle   = 0
+	moduleStatusMoving = 1
+)
+
+type packet struct {
+	srcDst int
+	pos    int
+	typ    packetType
+	status moduleStatus
+}
+
+func unmarshalPacket(raw []byte) packet {
+	p := packet{}
+	p.srcDst = int(raw[1])<<2 | int(raw[2])>>6
+	p.pos = ((int(raw[2]) & 0x3f) << 6) | (int(raw[3]) >> 2)
+	p.typ = packetType((raw[3] & 2) >> 1)
+	p.status = moduleStatus(raw[3] & 1)
+	return p
+}
+
+func (p packet) marshal() []byte {
+	ret := make([]byte, 5)
+	ret[0] = 0xd4
+	ret[1] = byte(p.srcDst >> 2)
+	ret[2] = byte(((p.srcDst & 0x3) << 6) | (p.pos >> 6))
+	ret[3] = byte((p.pos & 0x3f) << 2)
+	ret[3] |= byte((byte(p.typ) << 1) | byte(p.status))
+	ret[4] = crc8.Checksum(ret[:4], maximCRCTable)
+	return ret
+}
 
 func main() {
 	s, err := serial.OpenPort(&serial.Config{
@@ -19,11 +58,27 @@ func main() {
 		panic(err)
 	}
 
-	buf := []byte{0xd4, 0, 0, 2, 0}
-	buf[4] = crc8.Checksum(buf[:4], maximCRCTable)
-	fmt.Printf("%x %x %x %x\n", buf[1], buf[2], buf[3], buf[4])
-	//if _, err := s.Write(buf); err != nil {
-	//	panic(err)
-	//}
+	p := packet{
+		srcDst: 0,
+		pos:    0,
+		typ:    packetTypeCommand,
+	}
+	buf := p.marshal()
+	fmt.Printf("%+v\n", p)
+	p = unmarshalPacket(buf)
+
+	if _, err := s.Write(buf); err != nil {
+		panic(err)
+	}
+
+	time.Sleep(time.Second)
+
+	if _, err := s.Read(buf); err != nil {
+		panic(err)
+	}
+
+	p = unmarshalPacket(buf)
+	fmt.Printf("%+v\n", p)
+
 	s.Close()
 }
